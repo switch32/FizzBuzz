011f7cd56108b9f5f55bf1559f8982fdb3013ea8
switch32 <bjk.dot.h@hotmail.com>
2018-10-23 11:18:25 +0100

    Working version PART1

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,4 +1,4 @@
 var i;
 for (i = 1; i<101; i++)
-if (i % 5 == 0 && i % 3 == 0) {console.log('FizzBuzz'), i }
-if (i % 5 == 0) {console.log('Buzz', i)}
+if (i % 5 == 0 && i % 3 == 0) {console.log('FizzBuzz', i) }
+else if (i % 5 == 0) {console.log('Buzz', i)}
