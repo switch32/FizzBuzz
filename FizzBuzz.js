4f0e4350fca1dfad2824d1a870c6eecd6df92b45
switch32 <bjk.dot.h@hotmail.com>
2018-10-23 11:41:59 +0100

    Stuck

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,4 +1,8 @@
 var i;
 for (i = 1; i<101; i++)
 if (i % 5 == 0 && i % 3 == 0) {console.log('FizzBuzz', i) }
+else if (i % 5 == 0 && i % 13 == 0) {console.log('FezzBuzz', i)}
 else if (i % 5 == 0) {console.log('Buzz', i)}
+else if (i % 11 == 0) {console.log('Bong', i)}
+else if (i % 7 == 0 && (i % 3 ==0 || i % 5 == 0)) {console.log('FizzBang', i)}
+else if (i % 13 == 0) {console.log('Fezz', i)}
